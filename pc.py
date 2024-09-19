8a55e797586ca68b878599bcb5b9f6b719fa9da4
otov <voicehovicso@gmail.com>
2024-09-19 14:38:40 +0300

    last upd

--- pc.py
+++ pc.py
@@ -18,7 +18,7 @@ foto_label.grid(row=1,column=1,columnspan=2,padx=10,pady=10)
 
 def pasutijums():
     logs1=Toplevel()
-    logs1.geometry("400x400")
+    logs1.geometry("800x600")
     logs1.title("Pasūtījums")
     logs1.configure(background="purple")
 
@@ -39,17 +39,38 @@ def pasutijums():
 
     darbinieki["values"] = ("Mareks Bahmnais","Deniss Stepanovs", "Raimonds Leja", "Juris Kalniņš")
     prieksmets1["values"] = ("Monitors","Tastatūra", "Pele", "Austiņas", "Mikrofons", "Webcam")
-    cena1["values"] = ("5 €","10 €","20 €")
+    cena1["values"] = ("5","10","20")
 
     darbinieki.grid(row=1, column=2,pady=30, padx=10)
     prieksmets1.grid(row=2,column=2,pady=30, padx=10)
     cena1.grid(row=3,column=2,pady=30, padx=10)
     dienas1.grid(row=4, column=2,pady=30,padx=10)
+        
+
+    def rezultats():
+        selected_item3=cena1.get()
+        value = int(dienas1.get())
+        values = {"15":15, "55":55, "10":10}
+        cena = values[selected_item3]
+        result = value*cena
+        result_label.config(text=f"Result: {result} €")
 
 
+    result_label = tk.Label(logs1)
+    result_label.grid(row=5, column=2)
 
-btn=Button(logs, text="Pasūtījums", bd="8", command=lambda: pasutijums())
-btn1=Button(logs, text="Aizvērt", bd="8", command=logs.destroy)
+
+    btn2=tk.Button(logs1,text="Aprēķināt nomas cenu", bd="5", command= rezultats)
+    btn2.grid(row=5,column=1)
+
+    btn3=Button(logs1, text="Aizvērt", bd="5" ,command= logs1.destroy)
+    btn3.grid(row=5,column=3, pady=10, padx=10)
+
+    
+
+
+btn=Button(logs, text="Pasūtījums", bd="5", command=lambda: pasutijums())
+btn1=Button(logs, text="Aizvērt", bd="5", command=logs.destroy)
 
 btn.grid(row=1, column=4, pady=10, padx=10)
 btn1.grid(row=1, column=5, pady=10, padx=10)
